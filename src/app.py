f676b84b2c58f4e4ceea6b3c031e2b44537e62e1
David Reynolds <david.reynolds@hubbub.net>
2020-11-03 17:08:14 +0000

    Add jwt decoding to the container

--- src/app.py
+++ src/app.py
@@ -1,13 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import decode_jwt
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     with open('/etc/hostname', 'r') as fp:
         host = fp.read()
 
-    data = {
+    response_data = {
         'container_id': host.strip(),
     }
-    return jsonify(data)
+
+    authorization = request.headers.get('Authorization', None)
+    if authorization:
+        decoded_jwt = decode_jwt(authorization)
+        response_data['jwt'] = decoded_jwt
+    return jsonify(response_data)
